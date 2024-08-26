d25ae26b0041e7858240deeb2abc32bd89003751
Lucas Regalado <lucasregalado@hotmail.com>
2024-08-26 10:45:30 -0300

    hasta clase 200

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { PageNotFound } from "./ui/not-found/PageNotFound";
 export { ProductGrid } from "./products/products-grid/ProductGrid";
 export { ProductGridItem } from "./products/products-grid/ProductGridItem";
+export { Sidebar } from "./ui/sidebar/Sidebar";
 export { Title } from "./ui/title/Title";
 export { TopMenu } from "./ui/top-menu/Top-menu";
