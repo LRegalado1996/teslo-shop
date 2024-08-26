2538ead2bf861653f153730f4a6fc09daaa9aad6
Lucas Regalado <lucasregalado@hotmail.com>
2024-08-26 09:50:20 -0300

    clase 196

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,9 +1,14 @@
-import { Title } from "@/components";
+import { ProductGrid, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+
+const products = initialData.products;
 
 export default function ShopPage() {
   return (
     <>
       <Title title="Tienda" subtitle="Todos los productos" />
+
+      <ProductGrid products={products} />
     </>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { PageNotFound } from "./ui/not-found/PageNotFound";
+export { ProductGrid } from "./products/products-grid/ProductGrid";
 export { Title } from "./ui/title/Title";
 export { TopMenu } from "./ui/top-menu/Top-menu";
