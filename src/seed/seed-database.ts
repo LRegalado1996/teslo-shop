bb85d9e6e092e8b44c5f4c9119feb865fa115d0a
Lucas Regalado <lucasregalado@hotmail.com>
2024-08-27 16:25:50 -0300

    hasta clase 240

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,7 +1,41 @@
 import { initialData } from "./seed";
+import prisma from "../lib/prisma";
 
 async function main() {
-  console.log(initialData);
+  //1. Borrar registros previos
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
+
+  const { categories, products } = initialData;
+
+  //2. Agregar categorias
+  const categoriesData = categories.map((name) => ({ name }));
+  await prisma.category.createMany({ data: categoriesData });
+
+  const categoriesDB = await prisma.category.findMany();
+
+  const categoriesMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id;
+    return map;
+  }, {} as Record<string, string>);
+
+  //3. Agregar Productos
+  products.forEach(async (product) => {
+    const { type, images, ...rest } = product;
+
+    const ProductDB = await prisma.product.create({
+      data: {
+        ...rest,
+        categoryId: categoriesMap[type],
+      },
+    });
+
+    //Agregamos imagenes
+    const imagesData = images.map((url) => ({ url, productId: ProductDB.id }));
+    await prisma.productImage.createMany({ data: imagesData });
+  });
+
   console.log("Seed ejecutado correctamente");
 }
 
