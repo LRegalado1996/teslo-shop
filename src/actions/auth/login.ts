ac59965a40bba213e9ebfa95a8a0523368e3e6c0
Lucas Regalado <lucasregalado@hotmail.com>
2024-09-09 21:55:12 -0300

    Hasta clase 296

---
+++ src/actions/auth/login.ts
@@ -0,0 +1,11 @@
+"use server";
+
+import { signIn } from "@/auth.config";
+
+export async function authenticate(prevState: string | undefined, formData: FormData) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    return "Invalid credentials";
+  }
+}
